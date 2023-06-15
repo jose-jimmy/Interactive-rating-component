4a8bb27b689c8643ab68034dcc68ae1b79aaa5b1
Jose Jimmy <josejimmy8055@gmail.com>
2023-06-15 09:40:18 +0530

    javascript code modified

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const mainContainer = document.querySelector(".main-container");
 const thanksContainer = document.querySelector(".thankyou");
 const submitButton = document.querySelector(".submit_btn");
 const rating = document.getElementById("rating");
-const rates = document.querySelectorAll(".btn");
+const ratingContainer = document.querySelector(".rating");
 
 submitButton.addEventListener("click", () => {
   thanksContainer.classList.remove("hidden");
   mainContainer.style.display = "none";
 });
 
-rates.forEach((rate) => {
-  rate.addEventListener("click", () => {
-    rating.innerHTML = rate.innerHTML;
-  });
+ratingContainer.addEventListener("click", (event) => {
+  //Check if the clicked element is a button
+  if (event.target.nodeName === "BUTTON") {
+    rating.innerHTML = event.target.innerHTML;
+  }
 });
